b62be213df2ed87f919a22070266a12db221fa07
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-10-26 07:58:17 +0100

    feat: send breadcrumbid and processid in responses if set

--- fk-logging/src/main/java/se/fk/github/logging/callerinfo/filter/LoggingContextHttpResponseFilter.java
+++ fk-logging/src/main/java/se/fk/github/logging/callerinfo/filter/LoggingContextHttpResponseFilter.java
@@ -5,7 +5,11 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.ext.Provider;
+import org.slf4j.MDC;
+import se.fk.github.logging.callerinfo.model.HeaderTyp;
+import se.fk.github.logging.callerinfo.model.MDCKeys;
 import se.fk.github.logging.callerinfo.service.MDCPopulatorService;
 
 import java.io.IOException;
@@ -20,6 +24,22 @@ public class LoggingContextHttpResponseFilter implements ContainerResponseFilter
    @Override
    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException
    {
+      addIfNotPresent(responseContext.getHeaders(), HeaderTyp.BREADCRUMB_ID, MDCKeys.BREADCRUMBID);
+      addIfNotPresent(responseContext.getHeaders(), HeaderTyp.PROCESSID, MDCKeys.PROCESSID);
       mdcPopulatorService.clearMdc();
+   }
+
+   private void addIfNotPresent(MultivaluedMap<String, Object> headers, HeaderTyp header, MDCKeys mdcKey)
+   {
+      if (headers.containsKey(header.value()))
+      {
+         return;
+      }
+      String mdcValue = MDC.get(mdcKey.name());
+      if (mdcValue == null || mdcValue.isEmpty())
+      {
+         return;
+      }
+      headers.putSingle(header.value(), mdcValue);
    }
 }
